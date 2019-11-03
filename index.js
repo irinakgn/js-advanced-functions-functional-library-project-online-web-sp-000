967f55ac18a409ed29772c8bb82242153c0d6a40
Irina Kagan <irinagamb@gmail.com>
2019-11-03 20:00:53 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -4,23 +4,111 @@ const fi = (function() {
       return 'Start by reading https://medium.com/javascript-scene/master-the-javascript-interview-what-is-functional-programming-7f218c68b3a0'
     },
 
-    each: function() {
+    each: function(element, alert) {
+
+      if(Array.isArray(element)){
+        element.forEach((el)=> alert(el))
+        return element
+      } else {
+        Object.values(element).forEach((el)=> alert(el))
+        return element
+      }
 
     },
 
-    map: function() {
+    map: function(element, alert) {
+
+      if(Array.isArray(element)){
+        return element.map((el)=> alert(el))
+      } else {
+        return Object.values(element).map((el)=> alert(el))
+      }
+    },
+
+    reduce: function(element, callback, stPoint = 0) {
+      return element.reduce(callback, stPoint)
 
     },
 
-    reduce: function() {
-
-    },
-
-    functions: function() {
-
+    find: function(element, func) {
+      return element.find(func)
     },
 
 
+    filter: function(element, func) {
+
+      return element.filter(func)
+
+    },
+    filter: function(element, func) {
+
+      return element.filter(func)
+
+    },
+
+    size: function(elements) {
+
+      if(Array.isArray(elements)){
+        return elements.length
+      } else {
+        return Object.keys(elements).length
+      }
+
+    },
+    first: function(elements, elementsToGet) {
+      if(elementsToGet){
+        return elements.splice(0,3)
+      } else{
+        return elements[0]
+      }
+    },
+
+   last: function(elements, elementsToGet){
+     if(elementsToGet){
+       return elements.slice(1).slice(-3)
+     } else{
+       return elements.slice(-1)[0]
+     }
+   },
+
+    compact: function(elements){
+       return elements.filter(Boolean)
+
+   },
+
+
+
+   sortBy: function(elements){
+
+
+
+
+
+ },
+
+ flatten: function(elements){
+   return elements.flatten(func)
+
+
+},
+
+uniq: function(elements, elementsToGet){
+
+
+},
+keys: function(obj){
+return Object.keys(obj)
+
+},
+
+values: function(obj){
+return Object.values(obj)
+
+},
+Functions: function(elements, elementsToGet){
+
+
+},
   }
 })()
 
